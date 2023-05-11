255cd5e1bf551e479af9f59c0186ee1c814ee596
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-05-10 21:45:55 -0600

    Solve Game4 puzzle

--- test/game4Test.js
+++ test/game4Test.js
@@ -5,15 +5,16 @@ describe('Game4', function () {
   async function deployContractAndSetVariables() {
     const Game = await ethers.getContractFactory('Game4');
     const game = await Game.deploy();
+    const [account1] = await ethers.getSigners();
 
-    return { game };
+
+    return { game, account1 };
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, account1 } = await loadFixture(deployContractAndSetVariables);
 
-    // nested mappings are rough :}
-
-    await game.win();
+    await game.connect(account1).write(account1.address);
+    await game.win(account1.address);
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
