a939bec89dec354cb960446d8c439f8a24f30a6b
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-05-10 22:21:24 -0600

    Solved Game5 puzzle

--- test/game5Test.js
+++ test/game5Test.js
@@ -1,5 +1,7 @@
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+const { SignerWithAddress } = require('@nomiclabs/hardhat-ethers/signers');
 const { assert } = require('chai');
+const { ethers } = require('hardhat');
 
 describe('Game5', function () {
   async function deployContractAndSetVariables() {
@@ -10,10 +12,16 @@ describe('Game5', function () {
   }
   it('should be a winner', async function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
+    const provider = ethers.provider;
+    // the secret key reverted from randomly generated address in the internet resource https://www.ethereumaddressgenerator.com/
+    const wallet = new ethers.Wallet("c3818511b7743f8a557789c2e03fadb159a140e421ed2f1f1c5cc93bf70cdbef", provider);
+    const signer = ethers.provider.getSigner(0);
+    signer.sendTransaction({
+      to: wallet.address,
+      value: ethers.utils.parseEther('1.0')
+    })
 
-    // good luck
-
-    await game.win();
+    await game.connect(wallet).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
